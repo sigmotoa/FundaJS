c0d9ef4dc6dd4bae58089d475f4ceb816c0a6e2f
sigmotoa <sergio@newingenieros.com>
2019-06-18 11:48:26 -0500

    manejo de strings

--- clase1.js
+++ clase1.js
@@ -1,3 +1,19 @@
 var nombre='sigmotoa'
 var apellido='New Concept'
-console.log('Hola '+nombre+' '+apellido)
+
+//Atributos de Strings
+
+//MayusMinus
+var nombreEnMayusculas=nombre.toLocaleUpperCase()
+var apellidoEnMinusculas=apellido.toLowerCase()
+//Letra en Posicion
+var primeraLetra=nombre.charAt(0)
+//CantidadCaracteres
+var cantidadLetras=nombre.length
+//concatenar
+var nombrecompleto =`${nombre} ${apellido}`
+
+var str= nombre.substr(1,2)
+//Desafio mostrar utima del nombre
+//console.log('La ultima letra del nombre es: ')
+//console.log(nombre.charAt(nombre.length-1))
