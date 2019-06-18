444caf0b75e95717d5e5d983d25c51c051084f5e
sigmotoa <sergio@newingenieros.com>
2019-06-18 17:09:35 -0500

    usando funciones

---
+++ clase3.js
@@ -0,0 +1,15 @@
+var nombre='Sergio',edad=29
+
+function imprimirEdad()
+{
+console.log(`${nombre} tiene ${edad} anos`)
+}
+
+imprimirEdad()
+
+function imprimirEdad(n,e)
+{
+console.log(`${n} tiene ${e} anos`)
+}
+
+imprimirEdad(nombre,edad)
