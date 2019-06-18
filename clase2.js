a1435c9a554334269f2be539e9b251c4080af54e
sigmotoa <sergio@newingenieros.com>
2019-06-18 16:46:27 -0500

    numeros

---
+++ clase2.js
@@ -0,0 +1,20 @@
+var edad=27
+edad+=1
+
+var peso = 75
+peso-=2
+
+var sanduche=1
+peso=peso+sanduche
+
+var jugarFutbol=3
+
+peso-=jugarFutbol
+
+var precioVino=200.3
+var total=precioVino*100*3/100
+
+//hacer uso de Modulo Matematicas
+
+total=Math.round(precioVino*100*3)/100
+console.log(total.toFixed(2))
