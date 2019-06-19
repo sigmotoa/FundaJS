67c46de861c0947f6e66464d3ea7db9e4932e6f6
sigmotoa <sergio@newingenieros.com>
2019-06-18 22:12:43 -0500

    Manejo de condiciones

--- clase4.js
+++ clase4.js
@@ -1,9 +1,65 @@
 var x=4, y='4'
 //Cuando se usa === se consulta que la igualdad es totalmente exacta
 var sergio={
-    nombre:'Sergio'
+    nombre:'Sergio',
+    apellido:'Motoa',
+    edad:29,
+    ingeniero: true,
+    cantante: false,
+    contador:false,
+    profesor:true,
+    musico:false,
+    antropologo:false
 }
+
+function imprimeProfe(persona)
+{
+    console.log(`${persona.nombre} es: `)
+
+    if(persona.ingeniero)
+    {
+        console.log('Ingeniero')
+    }
+    if(persona.cantante)
+    {
+        console.log('Cantante')
+    }
+    if(persona.contador)
+    {
+        console.log('Contador')
+    }
+    if(persona.profesor)
+    {
+        console.log('Profesor')
+    }
+    if(persona.musico)
+    {
+        console.log('Musico')
+    }
+    if(persona.antropologo)
+    {
+        console.log('Antropologo')
+    }
+
+}
+imprimeProfe(sergio)
+
+function esMayor(persona)
+{
+    if(persona.edad>=18)
+    {
+        console.log(`${persona.nombre} es mayor de edad `)
+    }
+    else
+    {
+        console.log(`${persona.nombre} es menor de edad `)
+    }
+}
+esMayor(sergio)
 var otro={
     nombre:'Sergio'
 }
 //Cuando se comparan objetos, se compara es la direcci[on de memoria
+
+
+
