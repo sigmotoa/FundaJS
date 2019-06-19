7787c3efee1f1002d16b6f0a3f7b0b77b84ec75a
sigmotoa <sergio@newingenieros.com>
2019-06-18 20:39:00 -0500

    igualdades

---
+++ clase4.js
@@ -0,0 +1,9 @@
+var x=4, y='4'
+//Cuando se usa === se consulta que la igualdad es totalmente exacta
+var sergio={
+    nombre:'Sergio'
+}
+var otro={
+    nombre:'Sergio'
+}
+//Cuando se comparan objetos, se compara es la direcci[on de memoria
